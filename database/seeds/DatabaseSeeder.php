b3d552cef56f04bebec8a5ec11db3cf155fc079d
Ken Vilar <kenvilar@gmail.com>
2018-04-01 19:46:46 +0800

    Truncate the entire table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Buyer;
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Buyer::truncate();
+        Category::truncate();
+        Product::truncate();
+        Seller::truncate();
+        Transaction::truncate();
+        User::truncate();
     }
 }
