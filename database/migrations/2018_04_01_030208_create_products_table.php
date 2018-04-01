c361d1bcc0ad326f93ed16d225b82c8e1f62e39a
Ken Vilar <kenvilar@gmail.com>
2018-04-01 16:08:20 +0800

    Migration columns for Products

--- database/migrations/2018_04_01_030208_create_products_table.php
+++ database/migrations/2018_04_01_030208_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Product;
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +16,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('seller_id')->unsigned();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVAILABLE);
+            $table->text('image');
             $table->timestamps();
         });
     }
