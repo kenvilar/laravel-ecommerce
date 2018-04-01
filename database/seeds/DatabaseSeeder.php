a2ed8109b4e89da5a90f901ea1c479f36772fe88
Ken Vilar <kenvilar@gmail.com>
2018-04-01 19:51:59 +0800

    Set statement for foreign key

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        
         Buyer::truncate();
         Category::truncate();
         Product::truncate();
