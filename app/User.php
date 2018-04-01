87917ef08c812328cdd3999e6661dd7d7ffa5752
Ken Vilar <kenvilar@gmail.com>
2018-04-01 13:58:15 +0800

    Add verified, verification_token and admin fields

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'verified', 'verification_token', 'admin'
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'verification_token',
     ];
 }
