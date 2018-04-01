62d62954efbbe7c7e0b3b6af6b953d2bceb12493
Ken Vilar <kenvilar@gmail.com>
2018-04-01 20:07:19 +0800

    Database seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-        
+
         Buyer::truncate();
         Category::truncate();
         Product::truncate();
@@ -27,5 +27,20 @@ class DatabaseSeeder extends Seeder
         Transaction::truncate();
         User::truncate();
         DB::table('category_product')->truncate();
+
+        $categoriesQuantity = 30;
+        $productsQuantity = 1000;
+        $transactionsQuantity = 200;
+        $usersQuantity = 200;
+
+        factory(User::class, $usersQuantity)->create();
+        factory(Product::class, $productsQuantity)->create()->each(function ($product) {
+            $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+
+            //Attach the category to the product
+            $product->categories()->attach($categories);
+        });
+        factory(Transaction::class, $transactionsQuantity)->create();
+        factory(Category::class, $categoriesQuantity)->create();
     }
 }
