8f445600c72126f9dc4c9c6de44ee182773fc628
Ken Vilar <kenvilar@gmail.com>
2018-04-01 19:38:46 +0800

    Implement TransactionFactory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -1,11 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Seller;
+use App\Buyer;
+use App\Transaction;
 
-$factory->define(App\Transaction::class, function (Faker $faker) {
+$factory->define(Transaction::class, function (Faker $faker) {
+    $seller = Seller::query()->has('products')->get()->random();
+    $buyer = Buyer::all()->except($seller->id)->random();
+
     return [
         'quantity' => $faker->numberBetween(1, 100),
-        'buyer_id' => \App\User::all()->random()->id,
-        'product_id' => \App\Product::all()->random()->id,
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
