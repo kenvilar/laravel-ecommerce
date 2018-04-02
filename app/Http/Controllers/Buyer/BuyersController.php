929a46650804b05d17b7cbca3682404767a3590f
Ken Vilar <kenvilar@gmail.com>
2018-04-02 10:01:01 +0800

    Show single buyer that has transactions

--- app/Http/Controllers/Buyer/BuyersController.php
+++ app/Http/Controllers/Buyer/BuyersController.php
@@ -49,7 +49,9 @@ class BuyersController extends Controller
      */
     public function show($id)
     {
-        //
+        $buyers = Buyer::query()->has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $buyers], 200);
     }
 
     /**
