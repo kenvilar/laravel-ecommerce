85ec2763f11b2742758cedbc831012e0bcea7b88
Ken Vilar <kenvilar@gmail.com>
2018-04-02 14:49:00 +0800

    Mutators and accessors

--- app/User.php
+++ app/User.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class
+User extends Authenticatable
 {
     use Notifiable;
 
@@ -39,6 +40,18 @@ class User extends Authenticatable
         'remember_token',
         'verification_token',
     ];
+
+    public function setNameAttribute($name) {
+        $this->attributes['name'] = strtolower($name);
+    }
+    
+    public function getNameAttribute($name) {
+        return ucwords($name);
+    }
+
+    public function setEmailAttribute($email) {
+        $this->attributes['email'] = strtolower($email);
+    }
 
     public function isVerified()
     {
