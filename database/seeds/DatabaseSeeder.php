7c48f555f7510f9cf577c0f094db075e2f682fe0
Ken Vilar <kenvilar@gmail.com>
2018-04-02 09:42:51 +0800

    Fix the error during php artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use App\Category;
 use App\Product;
 use App\Transaction;
 use App\User;
+use App\Seller;
+use App\Buyer;
 use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
@@ -19,17 +21,22 @@ class DatabaseSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 
         User::truncate();
+        Seller::truncate();
+        Buyer::truncate();
         Category::truncate();
         Product::truncate();
         Transaction::truncate();
         DB::table('category_product')->truncate();
 
+        $usersQuantity = 1000;
         $categoriesQuantity = 30;
+        $sellersAndBuyersQuantity = 10;
         $productsQuantity = 1000;
         $transactionsQuantity = 1000;
-        $usersQuantity = 200;
 
         factory(User::class, $usersQuantity)->create();
+        factory(Seller::class, $sellersAndBuyersQuantity)->create();
+        factory(Buyer::class, $sellersAndBuyersQuantity)->create();
         factory(Category::class, $categoriesQuantity)->create();
         factory(Product::class, $productsQuantity)->create()->each(function ($product) {
             $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
