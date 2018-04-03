5659541741168b8db1af4458ebdfb9dfb771d39b
Ken Vilar <kenvilar@gmail.com>
2018-04-03 13:29:01 +0800

    bcrypt the secret

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,12 @@ use App\User;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
         'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
         'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
