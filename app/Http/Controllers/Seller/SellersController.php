fae6b06afdc69ec1b1db8bca64f0e1b14330ff79
Ken Vilar <kenvilar@gmail.com>
2018-04-03 11:30:54 +0800

    Resolve the model binding on SellersController show method that has dependecy method of has('products')

--- app/Http/Controllers/Seller/SellersController.php
+++ app/Http/Controllers/Seller/SellersController.php
@@ -15,10 +15,13 @@ class SellersController extends ApiController
         return $this->showAll($sellers);
     }
 
-    public function show($id)
+    public function show(Seller $seller)
     {
-        $seller = Seller::query()->has('products')->findOrFail($id);
-
+        //The original code of this method is
+        //$seller = Seller::query()->has('products')->findOrFail($id);
+        //meaning it has dependency method of has('products')
+        //So you need to create a App\Scopes\SellerScope and put it on the Seller Model with boot method
+        //to use and solve the laravel implicit model binding that has dependencies
         return $this->showOne($seller);
     }
 }
--- app/Seller.php
+++ app/Seller.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use App\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Seller extends Model
 {
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope);
+    }
+
     public function products()
     {
         return $this->hasMany(Product::class);
