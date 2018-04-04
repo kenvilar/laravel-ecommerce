ca56a31bd97c8468ad42bf258c4043eca89e5e08
Ken Vilar <kenvilar@gmail.com>
2018-04-04 21:05:16 +0800

    Implement the update method for SellersProductsController

--- app/Http/Controllers/Seller/SellersProductsController.php
+++ app/Http/Controllers/Seller/SellersProductsController.php
@@ -8,6 +8,7 @@ use App\Seller;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellersProductsController extends ApiController
 {
@@ -39,13 +40,43 @@ class SellersProductsController extends ApiController
         return $this->showOne($product);
     }
 
-    public function update(Request $request, Seller $seller)
+    public function update(Request $request, Seller $seller, Product $product)
     {
-        //
+        $this->validate($request, [
+            'quantity' => 'integer|min:1',
+            'image' => 'image',
+            'status' => 'in:' . Product::AVAILABLE . ',' . Product::UNAVAILABLE,
+        ]);
+        
+        $this->checkSeller($seller, $product);
+        
+        $product->fill($request->only(['name', 'description', 'quantity']));
+
+        if ($request->has('status')) {
+            $product->status = $request->status;
+
+            if ($product->isAvailable() && $product->categories()->count() === 0) {
+                return $this->errorResponse('An available product must have at least one category.', 409);
+            }
+        }
+
+        if ($product->isClean()) {
+            return $this->errorResponse('You need to specify a different value to update.', 422);
+        }
+
+        $product->save();
+        
+        return $this->showOne($product);
     }
 
     public function destroy(Seller $seller)
     {
         //
+    }
+
+    private function checkSeller(Seller $seller, Product $product) {
+        if ($seller->id !== $product->seller_id) {
+            throw new HttpException(422, 'The specified seller is not the actual seller of a product.');
+        }
     }
 }
