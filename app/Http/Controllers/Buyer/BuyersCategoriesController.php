e14d162d71b2cc3d43fc88f30c446266bbafec69
Ken Vilar <kenvilar@gmail.com>
2018-04-04 11:04:32 +0800

    Collapse the collection of items into a single array

--- app/Http/Controllers/Buyer/BuyersCategoriesController.php
+++ app/Http/Controllers/Buyer/BuyersCategoriesController.php
@@ -12,7 +12,7 @@ class BuyersCategoriesController extends ApiController
     public function index(Buyer $buyer)
     {
         $categories = $buyer->transactions()->with('product.categories')->get()
-            ->pluck('product.categories')->unique('id')->values();
+            ->pluck('product.categories')->collapse()->unique('id')->values();
 
         return $this->showAll($categories);
     }
