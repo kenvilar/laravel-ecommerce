fb9011bd17890ebf24116bf36259a81a96f1f3ee
Ken Vilar <kenvilar@gmail.com>
2018-04-04 18:08:41 +0800

    Unique id

--- app/Http/Controllers/Seller/SellersTransactionsController.php
+++ app/Http/Controllers/Seller/SellersTransactionsController.php
@@ -10,7 +10,7 @@ class SellersTransactionsController extends ApiController
     public function index(Seller $seller)
     {
         $transactions = $seller->products()->has('transactions')->with('transactions')->get()
-            ->pluck('transactions')->collapse()->unique()->values();
+            ->pluck('transactions')->collapse()->unique('id')->values();
 
         return $this->showAll($transactions);
     }
