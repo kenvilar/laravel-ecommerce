2dc6c8c25fa3808ca9bbf69a3c6b3da85be63763
Ken Vilar <kenvilar@gmail.com>
2018-04-04 10:47:46 +0800

    Reset the keys on the underlying array

--- app/Http/Controllers/Buyer/BuyersSellersController.php
+++ app/Http/Controllers/Buyer/BuyersSellersController.php
@@ -12,7 +12,7 @@ class BuyersSellersController extends ApiController
     public function index(Buyer $buyer)
     {
         $sellers = $buyer->transactions()->with('product.seller')->get()->pluck('product.seller')
-            ->unique('id');
+            ->unique('id')->values();
 
         return $this->showAll($sellers);
     }
