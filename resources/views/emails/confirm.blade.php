bfda78357550c76970a81e29ba058355027d3c8d
Ken Vilar <kenvilar@gmail.com>
2018-04-06 16:07:32 +0800

    Revert back

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,3 +1,3 @@
 Hello {{ $user->name }}!
 You've changed your email, so we need you to verify this email address. Please click the link below
-{{ route('resend', $user->verification_token) }}
+{{ route('verify', $user->verification_token) }}
