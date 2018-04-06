2f06449591975df513f2d1d8d5e9f5ca2bcefffe
Ken Vilar <kenvilar@gmail.com>
2018-04-06 23:17:22 +0800

    Return the data to the api with the new index key names from the transformer

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -19,16 +19,35 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
+        if (empty($collection) || $collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
+
+        $transformer = $collection->first()->transformer;
+
+        $collection = $this->transformData($collection, $transformer);
+
         return $this->successResponse(['data' => $collection], $code);
     }
 
     protected function showOne(Model $model, $code = 200)
     {
+        $transformer = $model->transformer;
+
+        $model = $this->transformData($model, $transformer);
+
         return $this->successResponse(['data' => $model], $code);
     }
 
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function transformData($data, $transformer)
+    {
+        $transformation = fractal($data, new $transformer);
+
+        return $transformation->toArray();
     }
 }
