78ec5e03faa352b1f7a68b850a12f0441f8525e3
Ken Vilar <kenvilar@gmail.com>
2018-04-08 07:36:47 +0800

    Laravel paginator

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 trait ApiResponser
@@ -28,6 +29,8 @@ trait ApiResponser
         $collection = $this->filterData($collection, $transformer);
 
         $collection = $this->sortData($collection, $transformer);
+
+        $collection = $this->paginate($collection);
 
         $collection = $this->transformData($collection, $transformer);
 
@@ -76,5 +79,22 @@ trait ApiResponser
         }
 
         return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+
+        $items = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($items, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 }
