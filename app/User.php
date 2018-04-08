4fb932d3f0d67c83a9d59b6a462581d70c1e313c
Ken Vilar <kenvilar@gmail.com>
2018-04-08 22:53:14 +0800

    Authenticate users thru api tokens

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use App\Transformers\UserTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     const VERIFIED_USER = '1';
     const UNVERIFIED_USER = '0';
