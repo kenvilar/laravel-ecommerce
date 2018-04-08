0d35366c87d485b47cca6da0b3abd64c29742ef4
Ken Vilar <kenvilar@gmail.com>
2018-04-08 11:23:57 +0800

    Fix categories.transactions index method

--- app/Http/Controllers/Category/CategoriesTransactionsController.php
+++ app/Http/Controllers/Category/CategoriesTransactionsController.php
@@ -9,8 +9,8 @@ class CategoriesTransactionsController extends ApiController
 {
     public function index(Category $category)
     {
-        $transactions = $category->products()->whereHas('transactions')->with('transactions')->get()
-            ->pluck('transactions');
+        $transactions = $category->products()->has('transactions')->with('transactions')->get()
+            ->pluck('transactions')->collapse();
 
         return $this->showAll($transactions);
     }
