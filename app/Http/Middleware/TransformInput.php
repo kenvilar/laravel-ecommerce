04676ad914b2991470d6d8344a2a1522ce55872c
Ken Vilar <kenvilar@gmail.com>
2018-04-08 13:17:14 +0800

    Fix TransformedInput for validation

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -9,12 +9,20 @@ class TransformInput
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $transformer)
     {
+        $transformedInput = [];
+
+        foreach ($request->request->all() as $key => $val) {
+            $transformedInput[$transformer::originalAttributes($key)] = $val;
+        }
+
+        $request->replace($transformedInput);
+
         return $next($request);
     }
 }
