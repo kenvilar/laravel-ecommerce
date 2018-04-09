e11e35233f7c0b77861893dc1515af03ef5e084c
Ken Vilar <kenvilar@gmail.com>
2018-04-09 19:27:06 +0800

    Create a management system for api clients

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         @if(Auth::check())
-                        <li><a href="{{ route('personal-tokens') }}">My Tokens</a></li>
+                            <li><a class="nav-link" href="{{ route('personal-tokens') }}">My Tokens</a></li>
+                            <li><a class="nav-link" href="{{ route('personal-clients') }}">My Clients</a></li>
+                            <li><a class="nav-link" href="{{ route('authorized-clients') }}">Authorized Clients</a></li>
                         @endif
                     </ul>
 
